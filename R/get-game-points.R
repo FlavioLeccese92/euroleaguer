a335d46398ba143b1954f08f106f7b6151a493bb
flavioleccese92 <flavioleccese92@gmail.com>
2024-01-28 01:19:33 +0100

    styling site

--- R/get-game-points.R
+++ R/get-game-points.R
@@ -2,7 +2,7 @@
 #'
 #' `r lifecycle::badge('experimental')`
 #'
-#' @param game_code The code of the game. An integer value.
+#' @param game_code The code of the game. An integer value
 #' @param season_code The code of the season (examples are "E2023" for Euroleague
 #' or "U2023" for Eurocup 2023)
 #' @return Returns scoring information of each player for the chosen games and seasons (subset of play-by-play data).
