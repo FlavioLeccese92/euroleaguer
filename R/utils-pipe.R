d2ebd1927a3d3b16e7939c7e36e74ba2d361941c
flavioleccese92 <flavioleccese92@gmail.com>
2024-01-29 10:10:56 +0100

    fixed badge cran

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -7,5 +7,5 @@
 NULL
 
 ## Quiets concerns of R CMD check re: the .'s and .data that appear in pipelines
-if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", ".data"))
+if (getRversion() >= "2.15.1")  utils::globalVariables(c(".", ".data"))
 
