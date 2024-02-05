5454005357495ee21ea1f801ad93beef99325866
flavioleccese92 <flavioleccese92@gmail.com>
2024-02-05 23:08:21 +0100

    added some glossary

--- R/get-game-box-score.R
+++ R/get-game-box-score.R
@@ -12,6 +12,35 @@
 #' - **PlayerStats**. Statistics for each player in the game
 #' - **TeamStats**. Aggregated statistics for each team in the game
 #'
+#' Glossary of columns:
+#'
+#' | **Column name** | **Column extended name**  |
+#' | --------------- | ------------------------- |
+#' | GP              | Game player               |
+#' | GS              | Game started              |
+#' | MIN             | Minutes played            |
+#' | PTS             | Points scored             |
+#' | 2PM             | Two-pointers made         |
+#' | 2PA             | Two-pointers attempted    |
+#' | 2P%             | Two-point %               |
+#' | 3PM             | Three-pointers made       |
+#' | 3PA             | Three-pointers attempted  |
+#' | 3P%             | Three-point %             |
+#' | FTM             | Free throws made          |
+#' | FTA             | Free throws attempted     |
+#' | FT%             | Free-throw %              |
+#' | OREB            | Offensive rebounds        |
+#' | DREB            | Defensive rebounds        |
+#' | TREB            | Total rebounds            |
+#' | AST             | Assists                   |
+#' | STL             | Steals                    |
+#' | TO              | Turnovers                 |
+#' | BLK             | Blocks                    |
+#' | BLKA            | Blocks against            |
+#' | FC              | Personal fouls committed  |
+#' | FD              | Personal fouls drawn      |
+#' | PIR             | Performance Index Rating  |
+#'
 #' Reference webpage: [BoxScore](<https://www.euroleaguebasketball.net/euroleague/game-center/2023-24/crvena-zvezda-meridianbet-belgrade-ldlc-asvel-villeurbanne/E2023/1/#boxscore>)
 #' @examples
 #' getGameBoxScore(season_code = c("E2023", "U2023"), game_code = 1)
--- R/get-game-playbyplay.R
+++ R/get-game-playbyplay.R
@@ -6,11 +6,48 @@
 #'
 #' @return Returns a list of two elements for the chosen games and seasons
 #' - **PlayByPlaySummary**. Overall information about the games, teams involved and status (live or not)
-#' - **PlayByPlay**. Detailed information about the games, particularly **NumberOfPlay** and **TypeOfPlay**
+#' - **PlayByPlay**. Detailed information about the games, particularly **NumberOfPlay** and **PlayType**
+#'
+#' Glossary of `PlayType`:
+#'
+#' | **PlayType** | **PlayInfo**              |
+#' | ------------ | ------------------------- |
+#' | 2PA          | Missed Two Pointer        |
+#' | 2PM          | Two Pointer               |
+#' | 3PA          | Missed Three Pointer      |
+#' | 3PM          | Three Pointer             |
+#' | AG           | Shot Rejected             |
+#' | AS           | Assist                    |
+#' | BP           | Begin Period              |
+#' | C            | Coach Foul                |
+#' | CCH          | Coach Challenge           |
+#' | CM           | Foul                      |
+#' | CMT          | Technical Foul            |
+#' | CMTI         | Throw-In Foul             |
+#' | CMU          | Unsportsmanlike Foul      |
+#' | D            | Def Rebound               |
+#' | EG           | End Game                  |
+#' | EP           | End Period                |
+#' | FTA          | Missed Free Throw         |
+#' | FTM          | Free Throw In             |
+#' | FV           | Block                     |
+#' | IN           | In                        |
+#' | O            | Off Rebound               |
+#' | OF           | Offensive Foul            |
+#' | OUT          | Out                       |
+#' | RV           | Foul Drawn                |
+#' | ST           | Steal                     |
+#' | TO           | Turnover                  |
+#' | TOUT         | Time Out                  |
+#' | TOUT_TV      | TV Time Out               |
 #'
 #' Reference webpage: [PlayByPlay](<https://www.euroleaguebasketball.net/euroleague/game-center/2023-24/crvena-zvezda-meridianbet-belgrade-ldlc-asvel-villeurbanne/E2023/1/#play-by-play>)
 #' @examples
-#' getGamePlayByPlay(season_code = c("E2023", "U2023"), game_code = 1)$PlayByPlay
+#' PlayByPlay = getGamePlayByPlay(season_code = c("E2023", "U2023"), game_code = 1)
+#'
+#' PlayByPlay$PlayByPlaySummary |> head(5)
+#'
+#' PlayByPlay$PlayByPlay |> head(5)
 #'
 #' @name getGamePlayByPlay
 #' @rdname getGamePlayByPlay
