cfa537ec4995abe72390a1619b7af296314b90b2
flavioleccese92 <flavioleccese92@gmail.com>
2024-02-20 23:19:59 +0100

    commented some examples and reduced dimensions of double-logo

--- R/get-team-lead-stats.R
+++ R/get-team-lead-stats.R
@@ -39,12 +39,13 @@
 #'
 #' Reference webpage: [TeamLead](<https://www.euroleaguebasketball.net/euroleague/stats/key-stats-teams/?size=200&misc=HomeGames&seasonCode=E2023&category=FreeThrowsAttempted&seasonMode=Single&sortDirection=descending&aggregate=accumulated>)
 #' @examples
-#' TeamLeadStats = getTeamLeadStats(season_code = c("E2022", "E2023"), phase_type = "RS")
+#' if (FALSE) {
+#'   TeamLeadStats = getTeamLeadStats(season_code = c("E2022", "E2023"), phase_type = "RS")
 #'
-#' TeamLeadStats$TeamAccumulated |> head(5)
+#'   TeamLeadStats$TeamAccumulated |> head(5)
 #'
-#' TeamLeadStats$TeamAveragePerGame |> head(5)
-#'
+#'   TeamLeadStats$TeamAveragePerGame |> head(5)
+#' }
 #' @name getTeamLeadStats
 #' @rdname getTeamLeadStats
 #' @export
--- cran-comments.md
+++ cran-comments.md
@@ -1,5 +1,11 @@
 ## R CMD check results
 
-0 errors | 0 warnings | 1 note
+0 errors ✔ | 0 warnings ✔ | 1 note ✖
 
-* This is a new release.
+* checking installed package size:
+  installed size is  5.0Mb
+  sub-directories of 1Mb or more:
+    help   4.9Mb
+
+  This folder contains 3 plot images that are used in README.Rmd
+  Reducing size compromises reading quality
